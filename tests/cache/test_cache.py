d57616682618ba0c7b3c9ec5ef360cc016e6ac1b
Martin Kovacs <martin.k.kovacs@gmail.com>
2021-02-01 17:34:10 +0100

    first commit

---
+++ tests/cache/test_cache.py
@@ -0,0 +1,185 @@
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
+import json
+import time
+import unittest
+
+import wynncraft.cache as cache
+import wynncraft
+
+"""
+Some compare tests occasionally fail, while still being correct.
+Run 'python -m unittest tests.cahce.test_cache.<Failed test class>' to test it again.
+If it repeatedly fails, then please open a pull request on github.
+"""
+
+
+def compare(d1, d2):
+    d1.pop("request", d1.pop("timestamp", None))
+    d2.pop("request", d2.pop("timestamp", None))
+    return d1 == d2
+
+
+def run_test(function, *args):
+    timer = Timer()
+
+    for i in range(10):
+        exec(f"{function}{args}")
+    
+    elapsed_seconds = timer.end()
+    return elapsed_seconds <= 1.0
+
+
+class Timer:
+    def __init__(self):
+        self.start_seconds = time.time()
+
+    def end(self):
+        return time.time() - self.start_seconds
+
+
+class TestGuild(unittest.TestCase):
+    def test_list(self):
+        self.assertTrue(compare(wynncraft.Guild.list(), cache.Guild.list()))
+        self.assertTrue(run_test("cache.Guild.list"))
+
+    def test_stats(self):
+        self.assertTrue(compare(wynncraft.Guild.stats("Wynncraft"), cache.Guild.stats("Wynncraft")))
+        self.assertTrue(run_test("cache.Guild.stats", "Wynncraft"))
+
+
+class TestIngredient(unittest.TestCase):
+    def test_get(self):
+        self.assertTrue(compare(wynncraft.Ingredient.get("apple"), cache.Ingredient.get("apple")))
+        self.assertTrue(run_test("cache.Ingredient.get", "apple"))
+        
+    def test_list(self):
+        self.assertTrue(compare(wynncraft.Ingredient.list(), cache.Ingredient.list()))
+        self.assertTrue(run_test("cache.Ingredient.list"))
+
+    def test_search_name(self):
+        self.assertTrue(compare(wynncraft.Ingredient.search_name("apple"), cache.Ingredient.search_name("apple")))
+        self.assertTrue(run_test("cache.Ingredient.search_name", "apple"))
+
+    def test_search_tier(self):
+        self.assertTrue(compare(wynncraft.Ingredient.search_tier("3"), cache.Ingredient.search_tier("3")))
+        self.assertTrue(run_test("cache.Ingredient.search_tier", "3"))
+        
+    def test_search_level(self):
+        self.assertTrue(compare(wynncraft.Ingredient.search_level("100"), cache.Ingredient.search_level("100")))
+        self.assertTrue(run_test("cache.Ingredient.search_level", "100"))
+        
+    def test_search_skills(self):
+        self.assertTrue(compare(wynncraft.Ingredient.search_skills("^cooking,armouring"), cache.Ingredient.search_skills("^cooking,armouring")))
+        self.assertTrue(run_test("cache.Ingredient.search_skills", "^cooking,armouring"))
+        
+    def test_search_sprite(self):
+        self.assertTrue(compare(wynncraft.Ingredient.search_sprite("^id<100>,damage<100>"), cache.Ingredient.search_sprite("^id<100>,damage<100>")))
+        self.assertTrue(run_test("cache.Ingredient.search_sprite", "^id<100>,damage<100>"))
+        
+    def test_search_identifications(self):
+        self.assertTrue(compare(wynncraft.Ingredient.search_identifications("^speed<;>,poison<;>"), cache.Ingredient.search_identifications("^speed<;>,poison<;>")))
+        self.assertTrue(run_test("cache.Ingredient.search_identifications", "^speed<;>,poison<;>"))
+        
+    def test_search_item_only_ids(self):
+        self.assertTrue(compare(wynncraft.Ingredient.search_item_only_ids("^strength<100>,defence<100>"), cache.Ingredient.search_item_only_ids("^strength<100>,defence<100>")))
+        self.assertTrue(run_test("cache.Ingredient.search_item_only_ids", "^strength<100>,defence<100>"))
+        
+    def test_search_consumable_only_ids(self):
+        self.assertTrue(compare(wynncraft.Ingredient.search_consumable_only_ids("^duration<100>,charges<100>"), cache.Ingredient.search_consumable_only_ids("^duration<100>,charges<100>")))
+        self.assertTrue(run_test("cache.Ingredient.search_consumable_only_ids", "^duration<100>,charges<100>"))
+
+
+class TestItem(unittest.TestCase):
+    def test_database_category(self):
+        self.assertTrue(compare(wynncraft.Item.database_category("all"), cache.Item.database_category("all")))
+        self.assertTrue(run_test("cache.Item.database_category", "all"))
+        
+    def test_database_search(self):
+        self.assertTrue(compare(wynncraft.Item.database_search("Idol"), cache.Item.database_search("Idol")))
+        self.assertTrue(run_test("cache.Item.database_search", "Idol"))
+
+
+class TestLeaderboard(unittest.TestCase):
+    def test_guild(self):
+        self.assertTrue(compare(wynncraft.Leaderboard.guild("alltime"), cache.Leaderboard.guild("alltime")))
+        self.assertTrue(run_test("cache.Leaderboard.guild", "alltime"))
+        
+    def test_player(self):
+        self.assertTrue(compare(wynncraft.Leaderboard.player("alltime"), cache.Leaderboard.player("alltime")))
+        self.assertTrue(run_test("cache.Leaderboard.player", "alltime"))
+
+    def test_pvp(self):
+        self.assertTrue(compare(wynncraft.Leaderboard.pvp("alltime"), cache.Leaderboard.pvp("alltime")))
+        self.assertTrue(run_test("cache.Leaderboard.pvp", "alltime"))
+
+
+class TestNetwork(unittest.TestCase):
+    def test_server_list(self):
+        self.assertTrue(compare(wynncraft.Network.server_list(), cache.Network.server_list()))
+        self.assertTrue(run_test("cache.Network.server_list"))
+    
+    def test_player_sum(self):
+        self.assertTrue(compare(wynncraft.Network.player_sum(), cache.Network.player_sum()))
+        self.assertTrue(run_test("cache.Network.player_sum"))
+
+
+class TestRecipe(unittest.TestCase):
+    def test_get(self):
+        self.assertTrue(compare(wynncraft.Recipe.get("Boots-1-3"), cache.Recipe.get("Boots-1-3")))
+        self.assertTrue(run_test("cache.Recipe.get", "Boots-1-3"))
+    
+    def test_list(self):
+        self.assertTrue(compare(wynncraft.Recipe.list(), cache.Recipe.list()))
+        self.assertTrue(run_test("cache.Recipe.list"))
+
+    def test_search_type(self):
+        self.assertTrue(compare(wynncraft.Recipe.search_type("boots"), cache.Recipe.search_type("boots")))
+        self.assertTrue(run_test("cache.Recipe.search_type", "boots"))
+
+    def test_search_skill(self):
+        self.assertTrue(compare(wynncraft.Recipe.search_skill("cooking"), cache.Recipe.search_skill("cooking")))
+        self.assertTrue(run_test("cache.Recipe.search_skill", "cooking"))
+
+    def test_search_level(self):
+        self.assertTrue(compare(wynncraft.Recipe.search_level("^min<0>,max<100>"), cache.Recipe.search_level("^min<0>,max<100>")))
+        self.assertTrue(run_test("cache.Recipe.search_level", "^min<0>,max<100>"))
+
+    def test_search_durability(self):
+        self.assertTrue(compare(wynncraft.Recipe.search_durability("^min<0>,max<100>"), cache.Recipe.search_durability("^min<0>,max<100>")))
+        self.assertTrue(run_test("cache.Recipe.search_durability", "^min<0>,max<100>"))
+
+    def test_search_healthOrDamage(self):
+        self.assertTrue(compare(wynncraft.Recipe.search_healthOrDamage("^min<0>,max<100>"), cache.Recipe.search_healthOrDamage("^min<0>,max<100>")))
+        self.assertTrue(run_test("cache.Recipe.search_healthOrDamage", "^min<0>,max<100>"))
+
+    def test_search_duration(self):
+        self.assertTrue(compare(wynncraft.Recipe.search_duration("^min<0>,max<100>"), cache.Recipe.search_duration("^min<0>,max<100>")))
+        self.assertTrue(run_test("cache.Recipe.search_duration", "^min<0>,max<100>"))
+
+    def test_search_basicDuration(self):
+        self.assertTrue(compare(wynncraft.Recipe.search_basicDuration("^min<0>,max<100>"), cache.Recipe.search_basicDuration("^min<0>,max<100>")))
+        self.assertTrue(run_test("cache.Recipe.search_basicDuration", "^min<0>,max<100>"))
+
+
+class TestPlayer(unittest.TestCase):
+    def test_stats(self):
+        self.assertTrue(compare(wynncraft.Player.stats("Salted"), cache.Player.stats("Salted")))
+        self.assertTrue(run_test("cache.Player.stats", "Salted"))
+
+    def test_uuid(self):
+        self.assertTrue(compare(wynncraft.Player.uuid("Salted"), cache.Player.uuid("Salted")))
+        self.assertTrue(run_test("cache.Player.uuid", "Salted"))
+
+
+class TestSearch(unittest.TestCase):
+    def test_name(self):
+        self.assertTrue(compare(wynncraft.Search.name("Salted"), cache.Search.name("Salted")))
+        self.assertTrue(run_test("cache.Search.name", "Salted"))
+
+
+class TestTerritory(unittest.TestCase):
+    def test_list(self):
+        self.assertTrue(compare(wynncraft.Territory.list(), cache.Territory.list()))
+        self.assertTrue(run_test("cache.Territory.list"))
---
+++ tests/test_wynncraft.py
@@ -0,0 +1,155 @@
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../wynncraft")))
+
+import unittest
+
+import wynncraft
+
+invalid = "asdasdasdasdasd"
+
+
+class TestGuild(unittest.TestCase):
+    def test_list(self):
+        self.assertEqual(type(wynncraft.Guild.list()["guilds"]), list)
+
+    def test_stats(self):
+        self.assertEqual(wynncraft.Guild.stats("Wynncraft")["name"], "Wynncraft")
+        self.assertEqual(wynncraft.Guild.stats(invalid), {"error": "Guild not found"})
+
+
+class TestIngredient(unittest.TestCase):
+    def test_get(self):
+        self.assertEqual(wynncraft.Ingredient.get("apple")["code"], 200)
+
+    def test_list(self):
+        self.assertEqual(wynncraft.Ingredient.list()["code"], 200)
+
+    def test_search(self):
+        self.assertRaises(TypeError, wynncraft.Ingredient.search, invalid, "")
+    
+    def test_search_name(self):
+        self.assertEqual(wynncraft.Ingredient.search_name("apple")["code"], 200)
+
+    def test_search_tier(self):
+        self.assertEqual(wynncraft.Ingredient.search_tier("3")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Ingredient.search_tier, "100")
+
+    def test_search_level(self):
+        self.assertEqual(wynncraft.Ingredient.search_level("100")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Ingredient.search_level, "-1")
+
+    def test_search_skills(self):
+        self.assertEqual(wynncraft.Ingredient.search_skills("&cooking")["code"], 200)
+        self.assertEqual(wynncraft.Ingredient.search_skills("^cooking,armouring")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Ingredient.search_skills, "cooking")
+
+    def test_search_sprite(self):
+        self.assertEqual(wynncraft.Ingredient.search_sprite("&id<100>")["code"], 200)
+        self.assertEqual(wynncraft.Ingredient.search_sprite("^id<100>,damage<100>")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Ingredient.search_sprite, "id<100>,damage<100>")
+
+    def test_search_identifications(self):
+        self.assertEqual(wynncraft.Ingredient.search_identifications("&speed<0;100>")["code"], 200)
+        self.assertEqual(wynncraft.Ingredient.search_identifications("^speed<;>,poison<;>")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Ingredient.search_identifications, "id<100>,damage<100>")
+
+    def test_search_item_only_ids(self):
+        self.assertEqual(wynncraft.Ingredient.search_item_only_ids("&strength<100>")["code"], 200)
+        self.assertEqual(wynncraft.Ingredient.search_item_only_ids("^strength<100>,defence<100>")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Ingredient.search_item_only_ids, "strength<100>,defence<100>")
+
+    def test_search_consumable_only_ids(self):
+        self.assertEqual(wynncraft.Ingredient.search_consumable_only_ids("&duration<100>")["code"], 200)
+        self.assertEqual(wynncraft.Ingredient.search_consumable_only_ids("^duration<100>,charges<100>")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Ingredient.search_consumable_only_ids, "duration<100>,charges<100>")
+
+
+class TestItem(unittest.TestCase):
+    def test_database_category(self):
+        self.assertEqual(type(wynncraft.Item.database_category("all")["items"]), list)
+        self.assertRaises(TypeError, wynncraft.Item.database_category, invalid)
+        
+
+    def test_database_search(self):
+        self.assertEqual(type(wynncraft.Item.database_search("Idol")["items"]), list)
+
+
+class TestLeaderboard(unittest.TestCase):
+    def test_guild(self):
+        self.assertEqual(type(wynncraft.Leaderboard.guild("alltime")["data"]), list)
+
+    def test_player(self):
+        self.assertEqual(type(wynncraft.Leaderboard.player("alltime")["data"]), list)
+
+    def test_pvp(self):
+        self.assertEqual(type(wynncraft.Leaderboard.pvp("alltime")["data"]), list)
+
+
+class TestNetwork(unittest.TestCase):
+    def test_server_list(self):
+        self.assertEqual(len(wynncraft.Network.server_list().keys()) >= 2, True)
+    
+    def test_player_sum(self):
+        self.assertEqual(type(wynncraft.Network.player_sum()["players_online"]), int)
+
+
+class TestRecipe(unittest.TestCase):
+    def test_get(self):
+        self.assertEqual(wynncraft.Recipe.get("Boots-1-3")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Recipe.get, invalid)
+    
+    def test_list(self):
+        self.assertEqual(wynncraft.Recipe.list()["code"], 200)
+
+    def test_search(self):
+        self.assertRaises(TypeError, wynncraft.Recipe.search, invalid, "")
+
+    def test_search_type(self):
+        self.assertEqual(wynncraft.Recipe.search_type("boots")["code"], 200)
+
+    def test_search_skill(self):
+        self.assertEqual(wynncraft.Recipe.search_skill("cooking")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Recipe.search_skill, invalid)
+
+    def test_search_level(self):
+        self.assertEqual(wynncraft.Recipe.search_level("&min<0>")["code"], 200)
+        self.assertEqual(wynncraft.Recipe.search_level("^min<0>,max<100>")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Recipe.search_level, invalid)
+
+    def test_search_durability(self):
+        self.assertEqual(wynncraft.Recipe.search_durability("&min<0>")["code"], 200)
+        self.assertEqual(wynncraft.Recipe.search_durability("^min<0>,max<100>")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Recipe.search_durability, invalid)
+
+    def test_search_healthOrDamage(self):
+        self.assertEqual(wynncraft.Recipe.search_healthOrDamage("&min<0>")["code"], 200)
+        self.assertEqual(wynncraft.Recipe.search_healthOrDamage("^min<0>,max<100>")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Recipe.search_healthOrDamage, invalid)
+
+    def test_search_duration(self):
+        self.assertEqual(wynncraft.Recipe.search_duration("&min<0>")["code"], 200)
+        self.assertEqual(wynncraft.Recipe.search_duration("^min<0>,max<100>")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Recipe.search_duration, invalid)
+
+    def test_search_basicDuration(self):
+        self.assertEqual(wynncraft.Recipe.search_basicDuration("&min<0>")["code"], 200)
+        self.assertEqual(wynncraft.Recipe.search_basicDuration("^min<0>,max<100>")["code"], 200)
+        self.assertRaises(TypeError, wynncraft.Recipe.search_basicDuration, invalid)
+
+
+class TestPlayer(unittest.TestCase):
+    def test_stats(self):
+        self.assertEqual(wynncraft.Player.stats("Salted")["code"], 200)
+
+    def test_uuid(self):
+        self.assertEqual(wynncraft.Player.uuid("Salted")["code"], 200)
+
+
+class TestSearch(unittest.TestCase):
+    def test_name(self):
+        self.assertEqual(wynncraft.Search.name("Salted")["search"], "salted")
+
+
+class TestTerritory(unittest.TestCase):
+    def test_list(self):
+        self.assertEqual(type(wynncraft.Territory.list()), dict)
