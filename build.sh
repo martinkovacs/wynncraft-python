696e5b2fc5d3d352e2bd7051015bc7ac6f88156d
Martin Kovács <martin.k.kovacs@gmail.com>
2021-02-03 14:27:41 +0100

    add version number check to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+if [ -z "$1" ]; then
+    echo "No version number"
+    exit
+fi
+
 python3 -m pip install --user --upgrade setuptools wheel twine
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/wynncraft-$1*
