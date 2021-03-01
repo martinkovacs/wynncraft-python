64818346d69d09d3895d8b38816cba8088873c0a
Martin Kovács <martin.k.kovacs@gmail.com>
2021-03-01 15:07:51 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wynncraft",
-    version="0.2.0",
+    version="0.2.1",
     author="Martin Kovács",
     author_email="martin.k.kovacs@gmail.com",
     description="A wrapper for the Wynncraft API.",
