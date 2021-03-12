e7c57a7f434571577c260f707c73f47feabf0820
Martin Kovács <martin.k.kovacs@gmail.com>
2021-03-12 15:28:52 +0100

    version number to 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wynncraft",
-    version="0.2.2",
+    version="0.2.3",
     author="Martin Kovács",
     author_email="martin.k.kovacs@gmail.com",
     description="A wrapper for the Wynncraft API.",
