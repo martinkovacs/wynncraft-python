01dcfa65e285fd1b008ec2de1b929d1694764387
Martin Kovács <martin.k.kovacs@gmail.com>
2021-03-13 18:16:41 +0100

    fixed api key handling

--- wynncraft/utils/request.py
+++ wynncraft/utils/request.py
@@ -9,8 +9,10 @@ def open(url):
     for char in url:
         if char in utils.constants.URL_CODES:
             url = url.replace(char, utils.constants.URL_CODES[char])
+
+    if utils.constants.URL_V1 in url:
+        url += f"&apikey={utils.constants.API_KEY}"
     
-    url += f"&apikey={utils.constants.API_KEY}"
     req = urllib.request.Request(url, headers={"apikey": utils.constants.API_KEY})
     
     res = urllib.request.urlopen(req, timeout=utils.constants.TIMEOUT)
