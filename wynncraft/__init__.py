eba1fdee57dfd81aed80dabeda4292cd2e4c97fb
Martin Kovács <martin.k.kovacs@gmail.com>
2021-03-14 15:48:57 +0100

    fixed missing API_KEY and RL_ENABLE variables

--- wynncraft/__init__.py
+++ wynncraft/__init__.py
@@ -1,5 +1,7 @@
+from wynncraft.utils.constants import API_KEY
 from wynncraft.utils.constants import CACHE_TIME
 from wynncraft.utils.constants import DEFAULT_TIMEOUT
+from wynncraft.utils.constants import RL_ENABLE
 
 import wynncraft.cache
 
